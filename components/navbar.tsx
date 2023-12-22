2d4fd4c20b0daa963d450e1c75c0009ca64352c9
Shamojinohito <shamo.922@gmail.com>
2023-12-22 07:11:11 +0100

    add simple navbar with menu icon without actual items

---
+++ components/navbar.tsx
@@ -0,0 +1,20 @@
+"use clinent";
+
+import { Menu } from "lucide-react"
+import Link from "next/link";
+
+export const Navbar = () => {
+    return ( 
+        <div className="fixed w-full z-50 flex justify-between item-center py-2 px-4 border-b border-primary/10 bg-secondary">
+            <div className="flex items-center">
+                <Menu className="block md:hidden"/>
+                <Link href="/">
+                    <h1 className="hidden md:block">
+                        companion.ai
+                    </h1>
+                </Link>
+            </div>
+        </div>
+     );
+}
+ 
