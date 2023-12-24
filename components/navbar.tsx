03095c25b3aa71d0f03091736a9b7d17ca25b6ec
Shamojinohito <shamo.922@gmail.com>
2023-12-24 23:23:46 +0100

    built sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,12 @@
 "use clinent";
 
 import { Menu, Sparkles } from "lucide-react"
-import {Poppins} from "next/font/google";
+import { Poppins } from "next/font/google";
 import Link from "next/link";
-import {UserButton} from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar"
 
 
 import {cn} from "@/lib/utils";
@@ -19,9 +20,9 @@ const font = Poppins({
 
 export const Navbar = () => {
     return ( 
-        <div className="fixed w-full z-50 flex justify-between item-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between item-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
-                <Menu className="block md:hidden"/>
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn(
                         "hidden md:block text-xl md:text-3xl font-bold text-primary",
