0c80909e654cd2647856859e053b7ed98f3b9421
Shamojinohito <shamo.922@gmail.com>
2023-12-26 23:45:11 +0100

    working on search filter

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,7 +7,7 @@ const RootLayout = ({children}: {children: React.ReactNode;}) => {
         <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
             <Sidebar />
         </div>
-        <main className="md:pl-20 pt-10 h-full">
+        <main className="md:pl-20 pt-16 h-full">
         {children}
         </main>
     </div> );
