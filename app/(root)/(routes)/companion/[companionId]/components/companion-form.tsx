aa9cb411d08d4ae18783f2364bd349cdd3cf1a81
Shamojinohito <shamo.922@gmail.com>
2024-01-05 00:04:21 +0100

    daily commit working on companion creation form

---
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Companion, Category } from "@prisma/client";
+
+interface CompanionFormProps {
+    initialData: Companion | null;
+    categories: Category[];
+}
+
+export const CompanionForm = ( {initialData, categories}: CompanionFormProps) => {
+    return (
+        <div>
+            Companion form
+        </div>
+    )
+}
